dbb3cb4cd0d7ab550aec7238d9133577ca15196c
cozcelik <cozczelik@cozcelikel.localdomain>
2025-11-11 22:59:39 +0300

    argv

---
+++ fdf.h
@@ -0,0 +1,10 @@
+#ifndef FDF_H
+#define FDH_H
+
+char	**ft_split(char const *s, char c);
+int	ft_wordcount(char *str, char c);
+#endif
+
+
+
+
---
+++ ft_split.c
@@ -0,0 +1,94 @@
+#include <stdlib.h>
+#include <stddef.h>
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+int	ft_wordcount(char *str, char c)
+{
+	int	i;
+	int	count;
+
+	i = 0;
+	count = 0;
+	while (str[i] != '\0')
+	{
+		while (str[i] == c)
+			i++;
+		if (str[i] != '\0')
+		{
+			count++;
+			while (str[i] != c && str[i] != '\0')
+				i++;
+		}
+	}
+	return (count);
+}
+
+static char	*ft_copy(const char *s, int start, int end)
+{
+	char	*word;
+	int		i;
+
+	word = (char *)malloc(sizeof(char) * (end - start + 1));
+	if (!word)
+		return (NULL);
+	i = 0;
+	while (start < end)
+		word[i++] = s[start++];
+	word[i] = '\0';
+	return (word);
+}
+
+static void	free_all(char **arr, int j)
+{
+	while (j > 0)
+	{
+		j--;
+		free(arr[j]);
+	}
+	free(arr);
+}
+
+char	**ft_split(char const *s, char c)
+{
+	char	**result;
+	int		i;
+	int		j;
+	int		start;
+
+	if (!s)
+		return (NULL);
+	result = (char **)malloc(sizeof(char *) * (ft_wordcount((char *)s, c) + 1));
+    if (!result)
+		return (NULL);
+	i = 0;
+	j = 0;
+	while (s[i])
+	{
+		while (s[i] == c)
+			i++;
+		start = i;
+		while (s[i] && s[i] != c)
+			i++;
+		if (i > start)
+		{
+			result[j] = ft_copy(s, start, i);
+			if (!result[j])
+			{
+				free_all(result, j);
+				return (NULL);
+			}
+			j++;
+		}
+	}
+	result[j] = NULL;
+	return (result);
+}
